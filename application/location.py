3c58a209f5cb40518aa8e3a0663b68ab4c6b7874
st19116ki <82067416+st19116ki@users.noreply.github.com>
2022-10-28 15:10:45 +0900

    Update location.py

--- application/location.py
+++ application/location.py
@@ -2,14 +2,6 @@
 def get_teacher_locations(db):
 	pass
 
-# 教員が設定している場所の状態の一覧を返す
-def get_teacher_location_states(db,teacher_id):
-	pass
-
-#教員の場所の状態を登録
-def register_location_state(db,teacher_id, location_name):
-	pass
-
 #教員の場所の更新
-def update_teacher_location(db,teacher_id, location_id):
+def update_teacher_location(db,teacher_id, location_name):
 	pass
