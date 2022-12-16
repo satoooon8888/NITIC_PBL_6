0a07411f16ef205d4f0934c8593ebf1833f1614e
Google0927 <st19113tk@gm.ibaraki-ct.ac.jp>
2022-12-16 16:02:07 +0900

    html編集

---
+++ app/__init__.py
@@ -0,0 +1,59 @@
+from flask import Flask, redirect, url_for, flash, render_template, jsonify, request
+from flask_login import login_required, logout_user, current_user
+from .config import Config
+from .models import db, login_manager, get_teachers, update_teacher_location, get_teacher_locations
+from .oauth import blueprint
+from .cli import create_db
+
+
+app = Flask(
+    __name__, 
+    static_folder='static', 
+    static_url_path=''
+)
+app.config.from_object(Config)
+app.register_blueprint(blueprint, url_prefix="/login")
+app.cli.add_command(create_db)
+db.init_app(app)
+login_manager.init_app(app)
+
+@app.route("/")
+def index():
+    return redirect("/index.html")
+
+@app.route("/api/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("You have logged out")
+    return redirect(url_for("index"))
+
+@app.route("/api/teacher")
+def handle_teacher():
+    teachers = get_teachers()
+    teacher_dicts = []
+    for teacher in teachers:
+        teacher_dicts.append({
+            "id": teacher.id, 
+            "email": teacher.email, 
+            "location": teacher.location
+        })
+    return jsonify(teacher_dicts)
+
+@app.route("/api/teacher/location", methods=["POST"])
+@login_required
+def update_location():
+    update_teacher_location(current_user, int(request.form["location_id"]))
+    return "ok"
+
+@app.route("/api/teacher/location")
+@login_required
+def get_location():
+    locations = get_teacher_locations(current_user)
+    location_dicts = []
+    for loc in locations:
+        location_dicts.append({
+            "user_id": loc.user_id,
+            "location": loc.location
+        })
+    return jsonify(location_dicts)
---
+++ app/models.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin
+from flask_dance.consumer.storage.sqla import OAuthConsumerMixin
+
+
+db = SQLAlchemy()
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), unique=True)
+    location_id = db.Column(db.Integer,db.ForeignKey('location.id'))
+
+class Location(db.Module):
+    __tablename__ = 'location'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    location = db.Column(db.String(100))
+
+class OAuth(OAuthConsumerMixin, db.Model):
+    __tablename__ = 'oauth'
+    provider_user_id = db.Column(db.String(256), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship(User)
+
+
+# setup login manager
+login_manager = LoginManager()
+login_manager.login_view = "google.login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+def get_teachers():
+    return User.query.all()
+
+def update_teacher_location(teacher, location_name):
+    teacher.location = location_name
+    db.session.commit()
+    
+def get_teacher_locations(teacher):
+    pass
