c6d2e13d240a334e47d1721f6b2928a07dad790f
satoooon8888 <samidare256@gmail.com>
2023-01-06 16:54:50 +0900

    add validation

--- app/oauth.py
+++ app/oauth.py
@@ -1,4 +1,4 @@
-from flask import flash
+from flask import flash, current_app
 from flask_login import current_user, login_user
 from flask_dance.contrib.google import make_google_blueprint
 from flask_dance.consumer import oauth_authorized, oauth_error
@@ -6,12 +6,12 @@ from flask_dance.consumer.storage.sqla import SQLAlchemyStorage
 from sqlalchemy.orm.exc import NoResultFound
 from .models import db, User, OAuth
 
+import re
 
 blueprint = make_google_blueprint(
     scope=["profile", "email"],
     storage=SQLAlchemyStorage(OAuth, db.session, user=current_user),
 )
-
 
 # create/login local user on successful OAuth login
 @oauth_authorized.connect_via(blueprint)
@@ -45,7 +45,17 @@ def google_logged_in(blueprint, token):
     else:
         # Create a new local user account for this user
         name = f"{info['family_name']} {info['given_name']}"
-        user = User(name=name, email=info["email"], location_id=None)
+        email = info["email"]
+
+        if current_app.config["ENABLE_EMAIL_VALIDATION"] == True:
+            # 茨城高専のメールアドレス以外は弾く
+            if not re.match("^.*@gm.ibaraki-ct.ac.jp$", email):
+                return False
+            # 生徒用のメールアドレスは弾く
+            if re.match("^st[0-9]{5}[a-z]{2}@gm.ibaraki-ct.ac.jp$", email):
+                return False
+
+        user = User(name=name, email=email, location_id=None)
         # Associate the new local user account with the OAuth token
         oauth.user = user
         # Save and commit our database models
